04fef00eb243e9818c016f0899558c0311dc85b3
Andrew <andrewmocol@hotmail.com>
2023-03-12 17:38:14 -0500

    update some stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,33 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const AnalyticsScripts = () => {
-  if (process.env.NODE_ENV !== 'production') return null
-  return (
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WTZFRN7')`,
-      }}
-    />
-  )
-}
+// const AnalyticsScripts = () => {
+//   if (process.env.NODE_ENV !== 'production') return null
+//   return (
+//     <Script
+//       dangerouslySetInnerHTML={{
+//         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','GTM-WTZFRN7')`,
+//       }}
+//     />
+//   )
+// }
 
-const AnalyticsPopup = () => {
-  if (process.env.NODE_ENV !== 'production') return null
-  return (
-    <noscript>
-      <iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-WTZFRN7"
-        height="0"
-        width="0"
-        style={{ display: 'none', visibility: 'hidden' }}
-      ></iframe>
-    </noscript>
-  )
-}
+// const AnalyticsPopup = () => {
+//   if (process.env.NODE_ENV !== 'production') return null
+//   return (
+//     <noscript>
+//       <iframe
+//         src="https://www.googletagmanager.com/ns.html?id=GTM-WTZFRN7"
+//         height="0"
+//         width="0"
+//         style={{ display: 'none', visibility: 'hidden' }}
+//       ></iframe>
+//     </noscript>
+//   )
+// }
 
 class MyDocument extends Document {
   render() {
@@ -74,13 +74,13 @@ class MyDocument extends Document {
           `,
             }}
           />
-          <AnalyticsScripts />
+          {/* <AnalyticsScripts /> */}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-        <AnalyticsPopup />
+        {/* <AnalyticsPopup /> */}
       </Html>
     )
   }
